957160446eb97e283a8cba6d722e2c6cd79ae40d
Sergio Méndez <sergioarm.gpl@gmail.com>
2020-08-10 16:43:50 -0600

    changes

--- python3-flask-traced/index_jaeger_child.py
+++ python3-flask-traced/index_jaeger_child.py
@@ -7,13 +7,29 @@ import opentelemetry.ext.requests
 from opentelemetry import trace
 from opentelemetry.ext import jaeger
 from opentelemetry.sdk.trace import TracerProvider
-from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import ConsoleSpanExporter
 from opentelemetry.sdk.trace.export import SimpleExportSpanProcessor
 from opentelemetry.ext.flask import FlaskInstrumentor
 
 from opentelemetry.sdk.trace.export import BatchExportSpanProcessor
-from opentelemetry.ext import http_requests
+from opentelemetry.ext.wsgi import OpenTelemetryMiddleware
+
+
+'''
+jaeger_exporter = jaeger.JaegerSpanExporter(
+    service_name="my-traced-service", agent_host_name="simplest-agent.observability.svc.cluster.local", agent_port=6831
+)
+
+trace.set_tracer_provider(TracerProvider())
+trace.get_tracer_provider().add_span_processor(
+    SimpleExportSpanProcessor(jaeger_exporter)
+)
+
+app = flask.Flask(__name__)
+FlaskInstrumentor().instrument_app(app)
+opentelemetry.ext.requests.RequestsInstrumentor().instrument()
+'''
+
 #--------------------------------------------------------------
 trace.set_tracer_provider(TracerProvider())
 tracer = trace.get_tracer(__name__)
@@ -38,14 +54,12 @@ span_processor = BatchExportSpanProcessor(jaeger_exporter)
 trace.get_tracer_provider().add_span_processor(span_processor)
 
 app = flask.Flask(__name__)
-
+app.wsgi_app = OpenTelemetryMiddleware(app.wsgi_app)#wsgi
 
 FlaskInstrumentor().instrument_app(app)
 opentelemetry.ext.requests.RequestsInstrumentor().instrument()
-
-http_requests.enable(trace.get_tracer_provider())
-
 #--------------------------------------------------------------
+
 @app.route("/example4")
 def span_attributes():
     tracer = trace.get_tracer(__name__)
@@ -53,7 +67,7 @@ def span_attributes():
         span1.set_attribute("attribute1","value1")
         with tracer.start_as_current_span("step2") as span2:
             r = requests.get("http://jaeger-app-srv-2:5555/example1")
-            span2.set_attribute("attribute1",str(request.headers))
+            span2.set_attribute("attribute1",str(requests.headers))
             span2.set_attribute("response",str(r))
     return "distribute context"
 
